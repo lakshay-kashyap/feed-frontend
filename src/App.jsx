2448f7fa57b142de1527b865041a4832fe221bc0
Lakshay Kashyap <lakshaykasyap.lk@gmail.com>
2026-03-26 16:16:53 +0530

    fix vercel routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import{BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import CreatePost from './pages/createPost.jsx'
 import Feed from './pages/Feed.jsx'
+import { Navigate } from "react-router-dom";
+
+
+
+
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path='/' element={<Navigate to="/feed" />} />
         <Route path='/create-post' element={<CreatePost/>} />
         <Route path='/feed' element={<Feed/>} />
       </Routes>
