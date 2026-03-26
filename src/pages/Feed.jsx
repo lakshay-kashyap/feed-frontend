ea48912193f1abe8b3205681c8bf9aa99e6c5df1
Lakshay Kashyap <lakshaykasyap.lk@gmail.com>
2026-03-26 16:00:15 +0530

    frontend ready

---
+++ src/pages/Feed.jsx
@@ -0,0 +1,52 @@
+
+import React, { useState,useEffect } from 'react'
+import axios from 'axios';
+const Feed = ()=> {
+    const API_URL = import.meta.env.VITE_API_URL;
+
+    const [posts, setposts] = useState([
+        {
+            image:"https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d",
+            caption:"A beautiful scenery"
+        }
+    ]);
+
+    useEffect(()=>{
+        axios.get(`${API_URL}/posts`)
+        .then((res)=>{
+            setposts(res.data.post)
+        }) 
+            
+        
+    },[])
+
+  return (
+     <section className='feed'>
+            <h1 className='decklist'>DeckLists</h1>
+         {posts.length > 0 ? (
+            posts.map((post,index)=>(
+                <div key={index} className='post'>
+                    <img src={post.image} alt={`Post ${index}`} />
+                    <p>{post.caption}</p>
+                    <button className='delete-btn' onClick={async()=>{
+                        await axios.delete(`${API_URL}/posts/${post._id}`)
+                        .then((res)=>{
+                            alert(res.data.message)
+                            setposts(posts.filter((p)=>p._id !== post._id))
+                        })
+                    }}>
+                        Delete
+                    </button>
+                </div>
+            ))
+         ) : (
+            <p>No posts available.</p>
+         )}
+          <button className='CreatePostBtn'>
+            <a href='/create-post'>Create Post</a>
+          </button>
+     </section>    
+  )
+}
+
+export default Feed
---
+++ src/pages/createPost.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
+
+const CreatePost = () => {
+  const API_URL = import.meta.env.VITE_API_URL;
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    const formData=new FormData(e.target);
+    await axios.post(`${API_URL}/create-post`,formData)
+    .then((res)=>{
+      navigate("/feed")
+    })
+    .catch((err)=>{
+      alert("Error creating post")
+    })
+  }
+
+  return (
+    <section className='create-post'>
+       <h1 className='createposth'>Create Post</h1>
+       <form onSubmit={handleSubmit}>
+        <input type="file" name='image' accept='image/*' />
+        <input type="text" name='caption' placeholder='Enter Caption' required/>
+        <button>Create Post</button>
+       </form>
+    </section>
+  )
+}
+
+export default CreatePost
